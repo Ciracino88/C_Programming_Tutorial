5845b9352afebb61f8d8a6a32dea0c9f4eacab61
Ciracino <didple81@gmail.com>
2024-11-01 09:10:23 +0900

    24.11.01 과제 업로드

--- C_Programming_Tutorial/ex/ex_9_7.c
+++ C_Programming_Tutorial/ex/ex_9_7.c
@@ -9,27 +9,27 @@
 
 #include <stdio.h>
 
-int isPrime(int n);
-
-int main_ex55(void) {
-    int num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-    
-    if (isPrime(num) == 1) {
-        printf("소수입니다.\n");
-    } else {
-        printf("소수가 아닙니다.\n");
-    }
-    return 0;
-}
-
-int isPrime(int n) {
-    for (int i = 2; i < n; i++) {
-        if (n % i == 0) {
-            return 0;
-        }
-    }
-    
-    return 1;
-}
+//int isPrime(int n);
+//
+//int main_ex55(void) {
+//    int num;
+//    printf("Enter a number: ");
+//    scanf("%d", &num);
+//    
+//    if (isPrime(num) == 1) {
+//        printf("소수입니다.\n");
+//    } else {
+//        printf("소수가 아닙니다.\n");
+//    }
+//    return 0;
+//}
+//
+//int isPrime(int n) {
+//    for (int i = 2; i < n; i++) {
+//        if (n % i == 0) {
+//            return 0;
+//        }
+//    }
+//    
+//    return 1;
+//}
